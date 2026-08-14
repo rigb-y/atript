0b80f8462e0a7d8bffcc065c312eff63df69dea3
Matt Warner <mattwarner744@gmail.com>
2026-08-14 11:05:05 -0500

    req + ignore

--- src/atript.py
+++ src/atript.py
@@ -22,7 +22,7 @@ from atript_secrets import (
                      get_claude_key
                      )
 
-# VWAP, RSI, EMA, ATR, bollinger Bands (20, 2\sigma)
+# VWAP, RSI, EMA, ATR, bollinger Bands (20, 2\sigma), MACD
 
 KEYS: dict[str,str] = {
         "R2_ACCOUNT_ID" : get_account_id(),
