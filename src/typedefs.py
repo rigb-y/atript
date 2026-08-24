5e00b845f418dbe96fce65ad5efc67071b6a5ac3
Matt Warner <mattwarner744@gmail.com>
2026-08-24 11:41:47 -0500

    typeddict for api params

--- src/typedefs.py
+++ src/typedefs.py
@@ -1,4 +1,15 @@
 from dataclasses import dataclass
+from typing import Required, TypedDict
 @dataclass
 class Limit:
     limit: int
+
+class MassiveParameters(TypedDict, total=False):
+    ticker: Required[str]
+    limit: int
+    sort: str
+    resolution: Required[str]
+    window_start: int
+    window_start_gte: str | int
+    window_start_lte: str | int
+
