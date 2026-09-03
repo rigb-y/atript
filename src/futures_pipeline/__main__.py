788116b3aaad8beac22f401941e9c366a41d794b
Matt Warner <mattwarner744@gmail.com>
2026-09-02 20:05:37 -0500

    validate resoultion

--- src/futures_pipeline/__main__.py
+++ src/futures_pipeline/__main__.py
@@ -6,6 +6,7 @@ from massive import RESTClient
 from .typedefs import MassiveParameters
 from .fetch import fetch
 from .preprocessing.preprocess import preprocess
+import re
 
 def main():
     settings: Settings = load_settings()
@@ -15,22 +16,24 @@ def main():
     if args.ticker is None:
         parser.error("-t/--ticker is required")
 
+    if args.resoultion:
+        if not (match := re.search(r"^(\d+)(sec|min|hour|session|week|month|quarter|year)$", args.resolution)):
+            raise ValueError("Resolution not in the correct form.")
+        if re.match(r"(month|quarter|year|session)", match.group(2)):
+            raise ValueError(f"{match.group(2)} is not yet supported. Currently supported periods are [min,sec,hour,day,week]")
 
+    resolution = args.resolution or settings.default_resolution
     match args.command:
         case "fetch":
             massive_parameters: MassiveParameters = {
                     # "limit": 100,
                     "sort": "window_start.desc",
-                    "resolution": (
-                        args.resolution
-                        if getattr(args, "resolution")
-                        else settings.default_resolution
-                        ),
+                    "resolution": resolution,
                     "ticker": args.ticker,
                     }
             fetch(massive_parameters, create_massive_client(settings.massive_api_key), args)
         case "preprocess":
-            preprocess(args.ticker)
+            preprocess(args.ticker, resolution)
         case _:
             return
     
