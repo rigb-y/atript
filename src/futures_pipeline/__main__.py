7b6bfe9f55b47cd14356bd6a690949dd18c4dde5
Matt Warner <mattwarner744@gmail.com>
2026-09-04 15:48:08 -0500

    added model to cli

--- src/futures_pipeline/__main__.py
+++ src/futures_pipeline/__main__.py
@@ -1,11 +1,11 @@
-from .config import Settings, load_settings
+from .config import Settings, load_settings, MODEL_DIR
 from .massive_client import create_massive_client
 from .cli import create_parser
 from argparse import ArgumentParser, Namespace
-from massive import RESTClient
 from .typedefs import MassiveParameters
 from .fetch import fetch
 from .preprocessing.preprocess import preprocess
+from .model import run_model
 import re
 
 def main():
@@ -16,7 +16,7 @@ def main():
     if args.ticker is None:
         parser.error("-t/--ticker is required")
 
-    if args.resoultion:
+    if args.resolution:
         if not (match := re.search(r"^(\d+)(sec|min|hour|session|week|month|quarter|year)$", args.resolution)):
             raise ValueError("Resolution not in the correct form.")
         if re.match(r"(month|quarter|year|session)", match.group(2)):
@@ -34,6 +34,21 @@ def main():
             fetch(massive_parameters, create_massive_client(settings.massive_api_key), args)
         case "preprocess":
             preprocess(args.ticker, resolution)
+        case "model":
+            hf_token = settings.hf_token
+            if (not args.target):
+                print(f"No target specified, using default target: {settings.default_target}")
+                target = settings.default_target
+            else:
+                target = args.target
+
+            if (not args.pred_length):
+                print(f"No prediction length specified, using default length of {settings.default_prediction_length}")
+                pred_length =  settings.default_prediction_length
+            else:
+                pred_length = args.pred_length
+
+            run_model(args.ticker, target, pred_length, hf_token=hf_token, model_dir=MODEL_DIR/args.ticker, store_weights=args.store_weights, eval=args.eval)
         case _:
             return
     
