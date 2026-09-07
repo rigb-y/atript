8574b701b361ac7f77bbc7797e16fed46a09ff10
Matt Warner <mattwarner744@gmail.com>
2026-09-07 14:31:14 -0500

    added Resolution class

--- src/futures_pipeline/typedefs.py
+++ src/futures_pipeline/typedefs.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 from typing import Required, TypedDict
-from pandas import Series
+from pandas import Series, DataFrame
+from .validate import CommandArgs, FetchLatestArgs, FetchRangeArgs, FetchLookbackArgs, ModelArgs, PreprocessArgs
+from datetime import date
+from typing import Literal
+import re
 
 @dataclass
 class Limit:
@@ -14,22 +18,66 @@ class MassiveParameters(TypedDict, total=False):
     window_start:  str
     window_start_gte: str
     window_start_lte: str
+    window_start_gt: str
+    window_start_lt: str
+
 
 @dataclass
 class EvaluationResult:
     pred_df: Series
     eval_df: Series 
+    by_horizon: DataFrame
     mae: float
     baseline_mae: float
     mae_skill: float
     rmse: float
     directional_accuracy: float
+    by_horizon_loss: DataFrame
     loss: dict[float, float]
     baseline_loss: dict[float,float]
+    intervals_by_horizon: DataFrame
     pi_coverage: float
     nominal_coverage: float
     mean_interval_width: float
     quantile_calibration: dict[float, float]
     calibration_error: dict[float, float]
 
+type InputArgs = (
+        CommandArgs 
+        | FetchLookbackArgs 
+        | FetchRangeArgs 
+        | FetchLatestArgs 
+        | ModelArgs 
+        | PreprocessArgs
+)
+
+type TimedeltaUnit = Literal["s", "min", "h", "D", "W"]
+
+class CandleResolution:
+    resolution: str
+    length: int
+    unit: str
+
+    __fixed_units: dict[str, TimedeltaUnit] = {
+            "sec": "s",
+            "min": "min",
+            "hour": 'h',
+            "day": "D",
+            "week": "W"
+    }
+
+    def __init__(self, resolution: str) -> None:
+        self.resolution = resolution
+        #  Parse resolution.
+        match = re.match(r"^(\d+)(sec|min|day|hour|session|week|month|quarter|year)$", resolution)
+    
+        if (match is None):
+            raise ValueError(f"{resolution} is not a valid resolution.")
+    
+        self.length = int(match.group(1))
+        self.unit = match.group(2)
+
+    # Maps resolution units to a unit from TimeDeltaUnitChoices.
+    def to_timedelta_unit(self) -> TimedeltaUnit:
+        return self.__fixed_units[self.unit]
 
