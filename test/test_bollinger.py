2ed30d3d58e5588ac268897fa2e6574f9e9256b3
Matt Warner <mattwarner744@gmail.com>
2026-09-07 21:08:29 -0500

    ...

--- test/test_bollinger.py
+++ test/test_bollinger.py
@@ -1,20 +1,32 @@
 import numpy as np
 import pandas as pd
 
+from datetime import datetime
 from futures_pipeline.preprocessing  import percent_b
-from futures_pipeline.datareader import load_latest_day
-from futures_pipeline.config import PROCESSED_DATA_DIR
+from futures_pipeline.datareader import load_latest_day, load_prior_data
+from futures_pipeline.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 ticker = "MESU6"
-data_path = f"{PROCESSED_DATA_DIR}/{ticker}"
+data_path = PROCESSED_DATA_DIR/ticker
 print(data_path)
-df = load_latest_day(data_path, "MESU6")
+# df = load_prior_data(data_path, "MESU6", "2026-08-27", "2026-08-28")
+df = load_prior_data(data_path, "MESU6", "2026-09-01", "2026-09-01")
 if (df is None):
     exit()
 
-print(df.groupby("ticker").cumcount())
+print(min(df['real_timestamp']))
+print(max(df['real_timestamp']))
+
+print(df.iloc[91])
+
+# Starts at 4pm CT and ends the following day at 5pm CT
+
+# print(df[df['real_timestamp'] == datetime.fromisoformat("2026-08-27T23:45:00Z")])
+# print(df[df['real_timestamp'] == datetime.fromisoformat("2026-08-28T00:00:00Z")])
+
+# print(df.head(30))
+
 
 # prices: pd.Series = df['close']
 # pct_b = percent_b(prices)
 # print(pct_b)
-
